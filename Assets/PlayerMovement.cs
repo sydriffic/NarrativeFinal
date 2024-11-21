b95ecacb9993eb7f402a10b62b3db7cf8966325a
s <spm9533@nyu.edu>
2024-11-21 13:35:57 -0500

    8D player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    public float speed;
+    Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Animate();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if(horizontal == 0 && vertical == 0)
+        {
+            rb.velocity = new Vector2(0, 0);
+            return;
+        }
+
+        input = new Vector2(horizontal, vertical);
+        rb.velocity = input * speed * Time.deltaTime;
+    }
+
+    void Animate()
+    {
+        anim.SetFloat("Xmove", input.x);
+        anim.SetFloat("Ymove", input.y);
+    }
+}
