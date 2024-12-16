559ab409f0a65cfddd3dc3caf87ca7b0d10de6af
HudsonJanow <hudsonjanow@gmail.com>
2024-12-15 19:01:16 -0500

    Almost fuckin done

---
+++ Assets/Elevator Move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class ElevatorMove : MonoBehaviour
+{
+    public bool IsTriggered = false;
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //IsTriggered = false;
+    }
+    public void ToggleIsTriggered()
+    { IsTriggered = !IsTriggered; } 
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if (IsTriggered)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+    }
+}
